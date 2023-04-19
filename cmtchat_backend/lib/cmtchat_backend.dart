1374125fe1084174b2a0f588560eb860d4fcc052
Pyret <tor88fg@gmail.com>
2023-04-19 04:59:33 +0200

    Created/imported library cmtchat_backend

---
+++ cmtchat_backend/lib/cmtchat_backend.dart
@@ -0,0 +1,16 @@
+library cmtchat_backend;
+
+export 'src/models/message.dart';
+export 'src/models/receipt.dart';
+export 'src/models/typing_event.dart';
+export 'src/models/user.dart';
+export 'src/services/encryption/encryption_contract.dart';
+export 'src/services/encryption/encryption_service_impl.dart';
+export 'src/services/message/message_service_contract.dart';
+export 'src/services/message/message_service_impl.dart';
+export 'src/services/receipt/receipt_service_contract.dart';
+export 'src/services/receipt/receipt_service_impl.dart';
+export 'src/services/typing/typing_notification_service_contract.dart';
+export 'src/services/typing/typing_notification_service_impl.dart';
+export 'src/services/user/user_service_contract.dart';
+export 'src/services/user/user_service_impl.dart';
