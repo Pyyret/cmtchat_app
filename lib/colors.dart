e8006d39ac0963c5c4212d320520c88c7c48d38f
Pyret <tor88fg@gmail.com>
2023-04-26 21:42:25 +0200

    implemented onboarding page with widgets

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kPrimary = Color(0xFFEF1B48);
+const kPrimary = Color(0xFF1788F2);
 const kBubbleLight = Color(0xFFE8E8E8);
 const kBubbleDark = Color(0xFF262629);
 const kAppBarDark = Color(0xFF111111);
--- lib/ui/pages/onboarding/onboarding.dart
+++ lib/ui/pages/onboarding/onboarding.dart
@@ -1,3 +1,7 @@
+import 'package:cmtchat_app/colors.dart';
+import 'package:cmtchat_app/ui/widgets/onboarding/logo.dart';
+import 'package:cmtchat_app/ui/widgets/onboarding/profile_upload.dart';
+import 'package:cmtchat_app/ui/widgets/shared/costum_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,9 +12,89 @@ class Onboarding extends StatefulWidget {
   State<StatefulWidget> createState() => _OnboardingState();
 }
 
-class _OnboardingState extends State<Onboarding>{
+class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _logo(context),
+              const Spacer(flex: 2,),
+              const ProfileUpload(),
+              const Spacer(flex: 1),
+              Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: CustomTextField(
+                    hint: 'Your name?',
+                    height: 45.0,
+                    onchanged: (val) {},
+                    inputAction: TextInputAction.done,
+                  ),
+              ),
+              const SizedBox(height: 30.0,),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 25.0,
+                  right: 25.0,
+
+                ),
+                child: ElevatedButton(
+                    onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: kPrimary,
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    )
+                  ),
+                    child: Container(
+                      height: 45.0,
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Create Chat',
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                          fontSize: 18.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _logo(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Cmt',
+            style: Theme.of(context)
+                .textTheme
+                .headlineMedium
+                ?.copyWith(fontWeight: FontWeight.bold)),
+        const SizedBox(width: 8.0),
+        const Logo(),
+        const SizedBox(width: 8.0),
+        Text('Chat',
+            style: Theme.of(context)
+                .textTheme
+                .headlineMedium
+                ?.copyWith(fontWeight: FontWeight.bold)),
+      ],
+    );
   }
 }
