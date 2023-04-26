95ce3999fc9db1b378554f962b9c3b84bb721228
Pyret <tor88fg@gmail.com>
2023-04-26 18:44:30 +0200

    started ui

---
+++ lib/ui/pages/onboarding/onboarding.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding>{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
